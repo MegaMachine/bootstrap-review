f350356dd35cf174168011cf80560e2b741fc843
liquidum <tolikdanylyuk@gmail.com>
2019-01-16 00:57:03 +0200

    Home

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,11 @@
 $('.hamburger--emphatic').click(function(){
   $(this).toggleClass('is-active');
 });
+
+var logo;
+$('.logo img').hover(function(){
+  logo =  $(this).attr('src');
+  $(this).attr('src','img/nike-red.svg');
+}, function(){
+  $(this).attr('src',logo);
+});
